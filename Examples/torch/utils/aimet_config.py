366665262fc31c69d681360cafafc6d8749b8b60
bruce1408 <summer56567@163.com>
2025-03-31 19:08:15 +0800

    Update .gitignore to include additional model and output file patterns

---
+++ Examples/torch/utils/aimet_config.py
@@ -0,0 +1,8 @@
+import time
+
+cuda_ids = "0, 1"
+
+imagenet_dir = "/mnt/share_disk/bruce_trie/outputs/imagenet_dataset"
+
+aimet_log_dir = "/mnt/share_disk/bruce_trie/Quantizer-Tools/aimet/output_log"
+
