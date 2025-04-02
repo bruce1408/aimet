24b3ed0c2b0ebfae4934cd263457b3efd75a5f48
bruce1408 <summer56567@163.com>
2025-04-02 23:16:50 +0800

    Update ONNX model path in config_param.py for consistency with workspace structure

--- Examples/common/config_param.py
+++ Examples/common/config_param.py
@@ -5,6 +5,6 @@ imagenet_dir = "/mnt/share_disk/bruce_trie/outputs/imagenet_dataset"
 
 aimet_log_dir = "/mnt/share_disk/bruce_trie/workspace/logs_aimet"
 
-onnx_resnet18_path = "/share/cdd/onnx_models/resnet18_official.onnx"
+onnx_resnet18_path = "/mnt/share_disk/bruce_trie/workspace/logs_aimet/resnet18_offical.onnx"
 
 imagenet_calibrate_dir = "/mnt/share_disk/bruce_trie/workspace/outputs/classify_calibration_images"
